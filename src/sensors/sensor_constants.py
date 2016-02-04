3120aeff020de546538af09930271612be1dfca1
Amine Mhedhbi <m.amine.mhedhbi@gmail.com>
2016-02-04 01:54:52 +0000

    fixed sensor_constants and its entropy to be used as a lib

--- src/sensors/sensor_constants.py
+++ src/sensors/sensor_constants.py
@@ -11,7 +11,8 @@ ONE_WIRE =   0x02
 INPUT =      'INPUT'
 OUTPUT =     'OUTPUT'
 
-# i2c devices type names as strings
+# i2c devices
+
 GYRO =       'Gyroscope'
 MAG =        'Magnetometer'
 RTC =        'Real-time Clock'
@@ -22,48 +23,69 @@ POWER =      'Power Sensor'
 
 I2C_DEVICES_LIST = [GYRO, MAG, RTC, TEMP, MUX, ADC, POWER]
 
-I2C_LOOKUP_TABLE = {
+I2C_DEVICES_LOOKUP_TABLE = {
     GYRO: {
-       "name": "ITG3205",
-       "address": 0x68,
-       "register": 
-       "quantity": 3,
-       "channels": [0, 1, 2]
+        'name': 'ITG3205',
+        'addr': 0x68,
+        'reg': {
+            'X-H': 0x1D,
+            'X-L': 0x1E,
+            'Y-H': 0x1F,
+            'Y-L': 0x20,
+            'Z-H': 0x21,
+            'Z-L': 0x22
+        },
+        'ch': [0, 1, 2]
     },
     MAG: {
-       "name": "HMC5883L",
-       "address": 0x1e,
-       "quantity": 3,
-       "channels": [0, 1, 2]
+        'name': 'HMC5883L',
+        'addr': 0x1e,
+        'reg': {
+            'X-H': 0x03,
+            'X-L': 0x04,
+            'Y-H': 0x05,
+            'Y-L': 0x06,
+            'Z-H': 0x07,
+            'Z-L': 0x08
+        },
+        'ch': [0, 1, 2]
     },
     RTC: {
-       "name": "DS3231",
-       "address": 0x68,
-       "quantity": 2,
-       "channels": [3, 4]
+        'name': 'DS3231',
+        'addr': 0x68,
+        'reg': {
+            'sec':   0x00,
+            'min':   0x01,
+            'hr':    0x02,
+            'day':   0x03,
+            'date':  0x04,
+            'month': 0x05,
+            'year':  0x07
+        },
+        'ch': [3, 4]
     },
     TEMP: {
-       "name": "DS1624",
-       "address": 0x48,
-       "quantity": 7,
-       "channels": [0, 1, 2, 3, 4, 5, 6, 7]
+        'name': "DS1624",
+        'addr': 0x48,
+        'reg': 0xAA,
+        'ch': [0, 1, 2, 3, 4, 5, 6]
     },
     MUX: {
-       "name": "TCA9548A",
-       "address": 0x70,
-       "quantity": 1,
-       "channels": [0]
+        'name': 'TCA9548A',
+        'addr': 0x70,
+        'reg': {},
+        'ch': [0]
     },
     ADC: {
-       "name": "AD128d818",
-       "address": 0x1D,
-       "quantity": 1,
-       "channels": [0]
+        'name': 'AD128d818',
+        'addr': 0x1D,
+        'reg': {},
+        'ch': []
     },
     POWER: {
-       "name": "INA219",
-       "address": -1,
-       "quantity": -1,
-       "channels": [-1]
+        'name': 'N/A',
+        'addr': -1,
+        'reg': {},
+        'ch': []
     }
 }
--- src/sensors/sensor_entropy.py
+++ src/sensors/sensor_entropy.py
@@ -2,15 +2,33 @@ from sensor_constants import *
 
 class SensorEntropy:
     @staticmethod
-    def _get_sensor_name (sensor_type):
+    def name (sensor_type):
         """
         returns the actual device name if sensor_type passed is valid,
         else, return 'None'
         """
         if sensor_type in I2C_DEVICES_LIST:
-            return I2C_LOOKUP_TABLE[sensor_type]["name"]
+            return I2C_DEVICES_LOOKUP_TABLE[sensor_type]["name"]
+        # else
+        return None
 
+    def addr(sensor_type):
+        if sensor_type in I2C_DEVICES_LIST:
+            return I2C_DEVICES_LOOKUP_TABLE[sensor_type]["addr"]
+        return None
+    
+    def reg(sensor_type):
+        if sensor_type in I2C_DEVICES_LIST:
+            return I2C_DEVICES_LOOKUP_TABLE[sensor_type]["reg"]
+        return None
+
+    def ch(sensor_type):
+        if sensor_type in I2C_DEVICES_LIST:
+            return I2C_DEVICES_LOOKUP_TABLE[sensor_type]["ch"]
         return None
 
 if __name__ == "__main__":
-    #print(SensorEntropy._get_sensor_name(GYRO))
+    print(SensorEntropy.name(GYRO))
+    print(SensorEntropy.addr(GYRO))
+    print(SensorEntropy.reg(GYRO))
+    print(SensorEntropy.ch(GYRO))
