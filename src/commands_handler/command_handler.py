e03be77ce228f45ade420759c895ad69485fc6a9
Olivier Nguyen <nguyenolive@gmail.com>
2016-02-07 13:53:54 -0500

    fixed up command handler

--- src/commands_handler/command_handler.py
+++ src/commands_handler/command_handler.py
@@ -5,58 +5,29 @@ import subprocess
 
 class CustomDate:
 
-    # months = ('JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG', \
-    # 'SEP', 'OCT', 'NOV','DEC')
+    def __init__(self, day, month, year, hour, minute, second):
+        self.day = str(day)
+        self.month = str(month)
+        self.year = str(year)
+        self.hour = str(hour)
+        self.minute = str(minute)
+        self.second = str(second)
 
-    # def __init__(self, day, month, year, hour, minute, second):
-    #
-    #     if 0 < day <= 31:
-    #         self.day = str(day)
-    #     else:
-    #         raise Exception("Invalid day specified")
-    #
-    #     if month in self.months:
-    #         self.month = month
-    #     else:
-    #         raise Exception("Invalid month specified")
-    #
-    #     self.year = str(year)
-    #
-    #     if 0 < hour <= 24:
-    #         self.hour = str(hour)
-    #     else:
-    #         raise Exception("Invalid hour specified")
-    #
-    #     if 0 < minute <= 60:
-    #         self.minute = str(minute)
-    #     else:
-    #         raise Exception("Invalid minute specified")
-    #
-    #     if 0 < second <= 60:
-    #         self.second = str(second)
-    #     else:
-    #         raise Exception("Invalid second specified")
+        # Construct proper date string
+        self.dateString = self.day + '/' + self.month + '/' + self.year \
+        + ' ' + self.hour + ':' + self.minute + ':' + self.second
 
-    def __init__(self, dateString):
         # Validate date string
-        datetime.datetime.strptime(dateString, '%d/%m/%Y %H:%M:%S')
-        self.dateString = dateString
+        datetime.datetime.strptime(self.dateString, '%d/%m/%Y %H:%M:%S')
 
-    # def generateDateCommand(self):
-    #     return self.day + ' ' + \
-    #      self.month + ' ' + self.year + ' ' + \
-    #     self.hour + ':' + self.minute + ':' + \
-    #     self.second
-
-    def generateDateCommand(self):
+    def generateDateString(self):
         return self.dateString
 
 class CommandHandler:
     @staticmethod
     def set_system_time(dateObj):
         if dateObj.__class__.__name__ == CustomDate.__name__:
-            # os.system(dateObj.generateDateCommand)
-            subprocess.call(['date', '-s', dateObj.generateDateCommand()])
+            subprocess.call(['date', '-s', dateObj.generateDateString()])
         else:
             raise TypeError("Invalid date object")
 
@@ -83,8 +54,7 @@ class CommandHandler:
         pass
 
 if __name__ == "__main__":
-    # date = CustomDate(2, 'OCT', 2006, 18, 9, 9)
-    # print(date.generateDateCommand())
-    # date = CustomDate('2/6/2016 22:07:13')
+    date = CustomDate(2, 9, 2006, 18, 9, 9)
+    print(date.generateDateString())
     # CommandHandler.set_system_time(date)
     print(CommandHandler.get_system_time())
