1593ea24e416f339795a4cb81d2a03a006fa603a
Amine Mhedhbi <m.amine.mhedhbi@gmail.com>
2016-02-11 04:05:47 +0000

    restructuring of the files in the project

---
+++ src/gui/handlers/systemhandlers/bak_constants.py
@@ -0,0 +1,10 @@
+"""
+File contains the constants used by the gui modules.
+"""
+
+SRC_PATH =                 "/root/csdc3/src/gui"
+LOGS_PATH =                SRC_PATH + "/logs"
+LOGS_CONFIG_SETUP_PATH =   LOGS_PATH + "/config_setup"
+PAYLOAD_PATH =             SRC_PATH + "/payload"
+POWER_MONITORING_PATH =    SRC_PATH + "/power_monitoring"
+SENSOR_PATH =              SRC_PATH + "/sensors"
