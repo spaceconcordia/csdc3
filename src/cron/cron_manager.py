813da199d519970a04bb71c8b8a95b56c44ba953
Justin <justin_bunker@hotmail.com>
2016-02-23 20:11:48 -0500

    modified cron manager

--- src/cron/cron_manager.py
+++ src/cron/cron_manager.py
@@ -45,7 +45,7 @@ class CronManager:
 
         self.jobList.append(job)
 
-    def remove_job(self, minute, hour, day, month, weekday, command):
+    def remove_job(self, command):
         """
         Method for removing existing jobs.
         """
@@ -58,43 +58,11 @@ class CronManager:
         if not isJobFound:
             raise Exception('Job does not exist and cannot be removed.')
 
-# class CronJob:
-#     def __init__(self, minute, hour, day, month, weekday, command):
-#         if minute == '*' or 0 <= int(minute) <= 59:
-#             self.minute = minute
-#         else:
-#             raise Exception("Minute out of range")
-#         if hour == '*' or 0 <= int(hour) <= 23:
-#             self.hour = hour
-#         else:
-#             raise Exception("Hour out of range")
-#         if day == '*' or 1 <= int(day) <= 31:
-#             self.day = day
-#         else:
-#             raise Exception("Day out of range")
-#         if month == '*' or 1 <= int(month) <= 12:
-#             self.month = month
-#         else:
-#             raise Exception("Month out of range")
-#         if weekday == '*' or 0 <= int(weekday) <= 6:
-#             self.weekday = weekday
-#         else:
-#             raise Exception("Weekday out of range")
-#
-#         self.command = command
-#
-#     def generate_command(self):
-#         return self.minute + ' ' + self.hour + ' ' + self.day + \
-#          ' ' + self.month + ' ' + self.weekday + ' ' + self.command + '\n'
-
-# Overall Structure
-# minute (0-59), hour (0-23, 0 = midnight), day (1-31),
-# month (1-12), weekday (0-6, 0 = Sunday), command
 
 if __name__ == "__main__":
     cronManager = CronManager()
     cronManager.add_or_update_job('1','*','*','*','*', 'python3 /root/csdc3/src/custom_gpio.py')
     cronManager.add_or_update_job('1','1','1','2','3', 'python3 /root/csdc3/src/custom_i2c.py')
     cronManager.add_or_update_job('1','1','1','2','5', 'python3 /root/csdc3/src/custom_i2c.py')
-    # cronManager.remove_job('1','1','1','2','3', 'python3 /root/csdc3/src/custom_i2c.py')
+    # cronManager.remove_job('python3 /root/csdc3/src/custom_gpio.py')
     cronManager.update_cron_file()
