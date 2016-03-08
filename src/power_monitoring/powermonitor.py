8d3b0be3edeb8baf7daf641b9ce81c78d9024d7b
Justin <justin_bunker@hotmail.com>
2016-03-08 17:04:30 -0500

    added template code for power monitoring

--- src/power_monitoring/powermonitor.py
+++ src/power_monitoring/powermonitor.py
@@ -1,6 +1,24 @@
 import sys
-sys.insert.path("/root/csdc3/sensors")
+sys.insert.path("/root/csdc3/src/sensors")
 from sensor_manager import SensorManager
 
 class PowerMonitor:
     def check_health(self):
+        # Get temperature inputs
+        tempIdentifiers = (TEMP_BAT_1, TEMP_BAT_2, TEMP_BAT_3, TEMP_BAT_4)
+        tempValues = []
+        for iden in tempIdentifiers:
+            SensorManager.init_temp_sensor(iden)
+            tempValue = SensorManager.read_temp_sensor(iden)
+            SensorManager.stop_temp_sensor(iden)
+            tempValues.append(tempValue)
+
+        # Perform logic
+        pass
+
+        # Output data
+        SensorManager.gpio_output(PSS_HTR_EN_1_GPIO, ON)
+        SensorManager.gpio_output(PSS_HTR_EN_2_GPIO, ON)
+        SensorManager.gpio_output(PSS_HTR_EN_3_GPIO, ON)
+        SensorManager.gpio_output(PSS_HTR_EN_4_GPIO, ON)
+        SensorManager.gpio_output(PSS_HR_MUX_SEL_GPIO, ON)
