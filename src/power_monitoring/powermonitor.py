abd39e563527f8f045294c3eb7ba6969995e9f76
Justin <justin_bunker@hotmail.com>
2016-03-09 00:36:38 -0500

    modifications

--- src/power_monitoring/powermonitor.py
+++ src/power_monitoring/powermonitor.py
@@ -1,32 +1,40 @@
 import sys
 sys.insert.path("/root/csdc3/src/sensors")
 from sensor_manager import SensorManager
+from statistics import median
 
 class PowerMonitor:
     def __init__(self):
         self.controlStatus = False
 
     def check_health(self):
+        """
+        Determines whether battery chargers must be set manually
+        """
         # Get temperature inputs
         tempIdentifiers = (TEMP_BAT_1, TEMP_BAT_2, TEMP_BAT_3, TEMP_BAT_4)
         tempValues = []
         for iden in tempIdentifiers:
             SensorManager.init_temp_sensor(iden)
-            tempValue = SensorManager.read_temp_sensor(iden)
+            valueList = []
+            # Get median of 5 value readings to remove outliers
+            for i in range(0,5):
+                valueList.append(SensorManager.read_temp_sensor(iden))
+            tempValue = median(valueList)
             SensorManager.stop_temp_sensor(iden)
+            # Keep final value of sensor
             tempValues.append(tempValue)
 
         # Get status identifiers
         statusIdentifiers = (PSS_HTR_STAT_1_GPIO, PSS_HTR_STAT_2_GPIO,\
         PSS_HTR_STAT_3_GPIO, PSS_HTR_STAT_4_GPIO)
         statusValues = []
+        for iden in statusIdentifiers:
+                statusValues.append(SensorManager.gpio_input(iden))
 
         # Define manual heater identifiers
         heaterIdentifers = (PSS_HTR_EN_1_GPIO, PSS_HTR_EN_2_GPIO,\
         PSS_HTR_EN_3_GPIO, PSS_HTR_EN_4_GPIO)
-
-        for iden in statusIdentifiers:
-                statusValues.append(SensorManager.gpio_input(iden))
 
         # Take control if required
         for i in range(0,len(tempValues)):
@@ -51,9 +59,15 @@ class PowerMonitor:
                 return
 
     def temp_threshold(self):
+        """
+        Computes the proper threshold value for the temperature
+        """
         return 8
 
     def is_battery_safe(self):
+        """
+        Determines if heaters have enough power to heat batteries
+        """
         return True
 
 
