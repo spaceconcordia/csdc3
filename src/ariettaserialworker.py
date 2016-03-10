5a00186d2d8d59d61eec35ffb2f85de7a59ae95f
Amine Mhedhbi <m.amine.mhedhbi@gmail.com>
2016-03-10 02:45:44 +0000

    quick fix

--- src/ariettaserialworker.py
+++ src/ariettaserialworker.py
@@ -3,22 +3,20 @@ import time
 import multiprocessing
 
 ## Change this to match your local settings
-SERIAL_PORT = '/dev/ttyUSB0'
+SERIAL_PORT = '/dev/ttyS0'
 SERIAL_BAUDRATE = 115200
 
 class SerialProcess(multiprocessing.Process):
 
-    def __init__(self, input_queue, output_queue):
+    def __init__(self):
         multiprocessing.Process.__init__(self)
-        self.input_queue = input_queue
-        self.output_queue = output_queue
         self.sp = serial.Serial(SERIAL_PORT, SERIAL_BAUDRATE, timeout=1)
 
     def close(self):
         self.sp.close()
 
     def writeSerial(self, data):
-        self.sp.write(data)
+        self.sp.write(data+"\n")
         # time.sleep(1)
 
     def readSerial(self):
@@ -27,21 +25,15 @@ class SerialProcess(multiprocessing.Process):
     def run(self):
         self.sp.flushInput()
         while True:
-            # look for incoming tornado request
-            if not self.input_queue.empty():
-                data = self.input_queue.get()
-                # send it to the serial device
-                self.writeSerial(data)
-                print("writing to serial: " + data)
             # look for incoming serial data
             if (self.sp.inWaiting() > 0):
                 data = self.readSerial()
                 print("reading from serial: " + data)
-                # send it back to tornado
-                self.output_queue.put(data)
+                # send it back as double the serial device
+                self.writeSerial(data+data)
 
 if __name__ == '__main__':
 	## start the serial worker in background (as a deamon)
-	sp = serialworker.SerialProcess(input_queue, output_queue)
+	sp = serialworker.SerialProcess()
 	sp.daemon = True
 	sp.start()
