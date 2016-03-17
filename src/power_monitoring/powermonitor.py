1ebfa02b05abab71d01acc68403ecf9e8e5dbe18
Justin <justin_bunker@hotmail.com>
2016-03-17 17:49:41 -0400

    fixed issue with hysterisis

--- src/power_monitoring/powermonitor.py
+++ src/power_monitoring/powermonitor.py
@@ -13,6 +13,7 @@ class PowerMonitor:
         self.payloadLock = Lock("/root/csdc3/src/utils/payloadLock.tmp")
         self.sensorReadingLock = \
         Lock("/root/csdc3/src/utils/sensorReadingLock.tmp")
+        self.pastReadingAboveThresh = True
 
     def check_health(self):
         """
@@ -21,7 +22,7 @@ class PowerMonitor:
         # Check if sensors are reading data in the system
         if areSensorsAcquiringData():
             return
-            
+
         # Get temperature inputs
         tempIdentifiers = (TEMP_BAT_1, TEMP_BAT_2, TEMP_BAT_3, TEMP_BAT_4)
         tempValues = []
@@ -60,31 +61,39 @@ class PowerMonitor:
 
         # Take control if required
         for i in range(0,len(tempValues)):
-            if tempValues[i] > self.temp_threshold() and statusValues[i] == 0:
+            if self.temp_threshold(tempValues[i], 'GT') and statusValues[i] == 0:
                 self.controlStatus = False
                 SensorManager.gpio_output(PSS_HTR_MUX_SEL_GPIO, ON)
                 return
-            elif tempValues[i] > self.temp_threshold() and statusValues[i] == 1:
+            elif self.temp_threshold(tempValues[i], 'GT') and statusValues[i] == 1:
                 self.controlStatus = True
                 SensorManager.gpio_output(PSS_HTR_MUX_SEL_GPIO, OFF)
                 SensorManager.gpio_output(heaterIdentifers[i], OFF)
                 return
-            elif tempValues[i] < self.temp_threshold() and statusValues[i] == 0:
+            elif self.temp_threshold(tempValues[i], 'LT') and statusValues[i] == 0:
                 self.controlStatus = True
                 SensorManager.gpio_output(PSS_HTR_MUX_SEL_GPIO, OFF)
                 if self.is_battery_safe():
                     SensorManager.gpio_output(heaterIdentifers[i], ON)
                 return
-            elif tempValues[i] < self.temp_threshold() and statusValues[i] == 1:
+            elif self.temp_threshold(tempValues[i] 'LT') and statusValues[i] == 1:
                 self.controlStatus = False
                 SensorManager.gpio_output(PSS_HTR_MUX_SEL_GPIO, ON)
                 return
 
-    def temp_threshold(self):
+    def temp_threshold(self, tempValue, sign):
         """
-        Computes the proper threshold value for the temperature
+        Returns boolean for crossed temperature threshold
         """
-        return 15
+        tempRef = 12
+        thresholdPct = 0.1
+        if sign == 'GT':
+            result = tempValue > tempRef*(1+thresholdPct)
+        elif sign == 'LT':
+            result = tempValue < tempRef*(1-thresholdPct)
+        else:
+            raise Exception("Invalid parameter: must be 'GT' or 'LT'")
+        return result
 
     def is_battery_safe(self):
         """
