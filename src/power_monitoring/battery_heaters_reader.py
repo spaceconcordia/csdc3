3705ad8a310f3fdb1ae4e4645420a4031de20b81
Space <Space>
2016-03-25 20:16:14 -0400

    mocking data for battery heater reader

--- src/power_monitoring/battery_heaters_reader.py
+++ src/power_monitoring/battery_heaters_reader.py
@@ -1,32 +1,35 @@
-import sys
-sys.path.append("/root/csdc3/src/sensors/")
-sys.path.append("/root/csdc3/src/utils/")
-from sensor_manager import SensorManager
-from sensor_constants import *
-from statistics import median
+# import sys
+# sys.path.append("/root/csdc3/src/sensors/")
+# sys.path.append("/root/csdc3/src/utils/")
+# from sensor_manager import SensorManager
+# from sensor_constants import *
+# from statistics import median
 
 def BatteryHeatersReader():
     # Get temperature inputs
-    tempIdentifiers = (TEMP_BAT_1,) # TEMP_BAT_2, TEMP_BAT_3, TEMP_BAT_4)
-    tempValues = []
-    for iden in tempIdentifiers:
-        SensorManager.init_temp_sensor(iden)
-        valueList = []
-        # Get median of 5 value readings to remove outliers
-        for i in range(0,5):
-            valueList.append(SensorManager.read_temp_sensor(iden))
-        tempValue = median(valueList)
-        print(tempValue)
-        SensorManager.stop_temp_sensor(iden)
-        # Keep final value of sensor
-        tempValues.append(tempValue)
+    # tempIdentifiers = (TEMP_BAT_1,) # TEMP_BAT_2, TEMP_BAT_3, TEMP_BAT_4)
+    # tempValues = []
+    # for iden in tempIdentifiers:
+    #     SensorManager.init_temp_sensor(iden)
+    #     valueList = []
+    #     # Get median of 5 value readings to remove outliers
+    #     for i in range(0,5):
+    #         valueList.append(SensorManager.read_temp_sensor(iden))
+    #     tempValue = median(valueList)
+    #     print(tempValue)
+    #     SensorManager.stop_temp_sensor(iden)
+    #     # Keep final value of sensor
+    #     tempValues.append(tempValue)
+    #
+    # # Get status identifiers
+    # statusIdentifiers = (PSS_HTR_STAT_1_GPIO, PSS_HTR_STAT_2_GPIO,\
+    # PSS_HTR_STAT_3_GPIO, PSS_HTR_STAT_4_GPIO)
+    # statusValues = []
+    # for iden in statusIdentifiers:
+    #         statusValues.append(SensorManager.gpio_input(iden,0))
 
-    # Get status identifiers
-    statusIdentifiers = (PSS_HTR_STAT_1_GPIO, PSS_HTR_STAT_2_GPIO,\
-    PSS_HTR_STAT_3_GPIO, PSS_HTR_STAT_4_GPIO)
-    statusValues = []
-    for iden in statusIdentifiers:
-            statusValues.append(SensorManager.gpio_input(iden,0))
+    tempValues = [27.2, 29.2, 31.2, 33.2]
+    statusValues = [True, None, False, True]
 
     # Set up dict containing result
     result = {"control": "OBC", "batteries": []}
@@ -40,7 +43,8 @@ def BatteryHeatersReader():
     return result
 
 def main():
-    print(BatteryHeatersReader())
+    result = BatteryHeatersReader()
+    print(result)
 
 if __name__ == '__main__':
     main()
