bbe0334694db4acfc09a387889b2b6e4d42749c0
Amine Mhedhbi <m.amine.mhedhbi@gmail.com>
2016-03-26 03:15:02 +0000

    added logging temp val and heaters state to a file

--- src/gui/handlers/systemhandlers/batteryinfohandler.py
+++ src/gui/handlers/systemhandlers/batteryinfohandler.py
@@ -6,6 +6,10 @@ from battery_heaters_reader import BatteryHeatersReader
 class BatteryInfoHandler(tornado.web.RequestHandler):
     def get(self):
         battery_data = BatteryHeatersReader()
+        f = open('/root/csdc3/src/gui/handlers/systemhandlers/battery_data.txt', 'a')
+        for battery in battery_data['batteries']:
+            f.write(str(battery['temp']) + ' ' + str(battery['heaters']) + '|')
+        f.write('\n')
         self.write({
             "status_code": 200,
             "data": battery_data
